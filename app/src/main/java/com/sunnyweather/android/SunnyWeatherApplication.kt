29a3f8c62a4b946a0971ee4e4d4759290b40842f
lmy200901 <lmy3520@live.com>
2021-03-01 10:11:30 +0800

    First commit.

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,22 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+
+        const val TOKEN = "l1PVXUgu5NsBoJab" // 填入你申请到的令牌值
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
